3ad4111f6395088c5259bf0feaf97388ce8c89a9
christofplie <97967480+christofplie@users.noreply.github.com>
2022-03-06 16:07:53 +0100

    Refactoring calc methode

--- app/src/main/java/com/example/einzelbeispielplieschnegger/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispielplieschnegger/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calc();
     }
 
     public void calc() {
@@ -26,23 +27,12 @@ public class MainActivity extends AppCompatActivity {
                 arrayList.add(Character.getNumericValue(matNr.charAt(i)));
             }
 
-            int num = 0;
-            int count = 0;
-
-            for (int a = 2; a <= 4; a++) {
+            for (int a = 2; a <= 9; a++) {
 
                 for (int i = 0; i < arrayList.size(); i++) {
 
-                    if (count == 0 && (arrayList.get(i) == 5 || arrayList.get(i) == 7)) {
-                        num = arrayList.get(i);
 
-                        for (int k = 0; k < arrayList.size(); k++) {
-                            if (arrayList.get(k) == num && k > i) {
-                                System.out.println(arrayList.get(i) + " + " + arrayList.get(k) + " -> " + " Position " + i + " Position " + k + " Divisor " + num);
-                            }
-                        }
-                        num = 0;
-                    } else if (arrayList.get(i) != 0 && arrayList.get(i) % a == 0) {
+                if (arrayList.get(i) != 0 && arrayList.get(i) % a == 0) {
                         for (int j = 0; j < arrayList.size(); j++) {
                             if (arrayList.get(j) != 0 && arrayList.get(j) % a == 0 && j > i) {
                                 System.out.println(arrayList.get(i) + " + " + arrayList.get(j) + " -> " + " Position " + i + " Position " + j + " Divisor " + a);
@@ -50,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }
-                count++;
             }
         }
     }
