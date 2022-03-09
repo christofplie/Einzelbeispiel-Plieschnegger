de491f2036b0e411ba36fb8b2d3aea0f492a6bd8
christofplie <97967480+christofplie@users.noreply.github.com>
2022-03-09 16:28:46 +0100

    Code/View verknüpft

--- app/src/main/java/com/example/einzelbeispielplieschnegger/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispielplieschnegger/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.einzelbeispielplieschnegger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,35 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calc();
     }
 
-    public void calc() {
-        String matNr = "12007599";
-        ArrayList<Integer> arrayList = new ArrayList<>();
+    public void calc(View view) {
+        EditText input = findViewById(R.id.inputMatnr);
+        TextView resultField = findViewById(R.id.resultField);
+        StringBuilder result = new StringBuilder();
 
-        if (matNr.length() != 8) {
-            System.out.println("Ungültige Matrikelnummer");
+        ArrayList<Integer> matrNr = new ArrayList<>();
+
+        if (input.length() != 8) {
+            resultField.setText("Ungültige Matrikelnummer");
         } else {
 
             for (int i = 0; i < 8; i++) {
-                arrayList.add(Character.getNumericValue(matNr.charAt(i)));
+                matrNr.add(Character.getNumericValue(input.getText().charAt(i)));
             }
 
             for (int a = 2; a <= 9; a++) {
 
-                for (int i = 0; i < arrayList.size(); i++) {
+                for (int i = 0; i < matrNr.size(); i++) {
 
+                    if (matrNr.get(i) != 0 && matrNr.get(i) % a == 0) {
 
-                if (arrayList.get(i) != 0 && arrayList.get(i) % a == 0) {
-                        for (int j = 0; j < arrayList.size(); j++) {
-                            if (arrayList.get(j) != 0 && arrayList.get(j) % a == 0 && j > i) {
-                                System.out.println(arrayList.get(i) + " + " + arrayList.get(j) + " -> " + " Position " + i + " Position " + j + " Divisor " + a);
+                        for (int j = 0; j < matrNr.size(); j++) {
+
+                            if (matrNr.get(j) != 0 && matrNr.get(j) % a == 0 && j > i) {
+                                result.append(matrNr.get(i) + " + " + matrNr.get(j) + " -> " + " Position " + i + " Position " + j + " Divisor " + a + "\n");
                             }
                         }
                     }
                 }
             }
+            resultField.setText(result);
         }
     }
 }
